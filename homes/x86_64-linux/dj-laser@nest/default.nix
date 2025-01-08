777b0d622a1a8939aba442e5b177f3fa8e6eafa2
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-01-08 15:55:00 -0500

    add bash + zoxide

--- homes/x86_64-linux/dj-laser@nest/default.nix
+++ homes/x86_64-linux/dj-laser@nest/default.nix
@@ -4,6 +4,11 @@
 
   programs.home-manager.enable = true;
 
+  djlaser = {
+    shells.bash.enable = true;
+    tools.zoxide.enable = true;
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "24.11";
 }
