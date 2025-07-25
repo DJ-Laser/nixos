9bf4df3c8a2f0eea2beb865fc39aff7033f6c307
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-07-25 15:13:50 -0400

    syncthing devices

--- modules/home/services/syncthing/default.nix
+++ modules/home/services/syncthing/default.nix
@@ -19,11 +19,23 @@ in {
       extraOptions = ["--no-default-folder"];
 
       settings = {
+        devices = {
+          nix-desktop = {
+            id = "NV2AV5N-BUXWOUG-2N7TISV-RVJXTUI-ETIT62B-QGH4XU6-E3VR37K-6KZDYA3";
+          };
+
+          laser-book = {
+            id = "TSEDB76-4JPPNAX-ZE5IVBR-2IOZYWR-SQIRNIN-JR4KSDK-OJB4GOG-UCOU4AK";
+          };
+        };
+
         folders = {
           "music" = {
             id = "music";
             path = "~/Music";
             label = "Music";
+
+            devices = ["nix-desktop" "laser-book"];
           };
         };
       };
