3ffe10039dab7007b547bd2c525e6f4c21c16795
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2026-01-16 09:42:38 -0500

    add silly button

--- modules/home/desktops/niri/config/binds/general.nix
+++ modules/home/desktops/niri/config/binds/general.nix
@@ -11,6 +11,10 @@ with actions; {
     hotkey-overlay.title = "Open the Alacritty terminal";
     action = spawn "alacritty";
   };
+  "Mod+Shift+T" = {
+    hotkey-overlay.title = "Silly :3";
+    action = spawn "alacritty" "-e" "${pkgs.cmatrix}/bin/cmatrix";
+  };
   "Mod+D" = {
     hotkey-overlay.title = "Open the N16 Launcher";
     action = spawn "n16" "launcher" "open";
